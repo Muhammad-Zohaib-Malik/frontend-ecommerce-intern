48f6e24f07305e17ef04ae263a80e5ca65308164
zohaib <muhammadzohaibmalik10@gmail.com>
2025-05-04 19:43:05 +0500

    add api

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Create an axios instance with default settings
-const API_BASE_URL = 'https://backend-ecommerce-intern.vercel.app/';
+const API_BASE_URL = 'https://backend-ecommerce-intern.vercel.app/api';
 // const API_BASE_URL='http://localhost:5000/api/';
 
 const apiClient = axios.create({
